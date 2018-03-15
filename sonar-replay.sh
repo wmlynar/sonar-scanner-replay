229838bf2db891ee7e34a3c686fbf52d365f0b96
Wojciech Mlynarczyk <wojciech.mlynarczyk+githug@gmail.com>
2018-03-15 21:14:47 +0100

    modified script to make branch configurable

--- sonar-replay.sh
+++ sonar-replay.sh
@@ -6,12 +6,13 @@ CONFIGURATION_FILE=/home/woj/tmp/sonar-project.properties
 PROJECT_REPO=https://github.com/wmlynar/extended-kalman-filter.git
 TMP_FOLDER=~/tmp/sonar-replay-tmp
 BUILD_COMMAND='mvn clean install'
+BRANCH=master
 
 rm -rf $TMP_FOLDER
 git clone $PROJECT_REPO $TMP_FOLDER
 cd $TMP_FOLDER
 
-git checkout master
+git checkout ${BRANCH}
 git pull
 
 commit_log=( $(git log --date=short | grep -e "Date:" | sed -E 's/commit //' | sed -E 's/Date:[ ]+//' | uniq | tac) )
@@ -21,7 +22,7 @@ for ((i=0; i<${#commit_log[*]}; i++)); do
 	commit_date=${commit_log[$i]}
 	echo "Date: ${commit_date}"
 
-	git checkout --force `git rev-list -n 1 --before="${commit_date}" master`
+	git checkout --force `git rev-list -n 1 --before="${commit_date}" ${BRANCH}`
 	$BUILD_COMMAND
 
 	cp --force ${CONFIGURATION_FILE} .
